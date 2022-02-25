6c763b4dd5183e8c9c0eb44d606e2b4d92c0f661
Rusakov Alex <alexanderwk@yandex.ru>
2022-02-25 14:26:15 +0300

    WebAdminSystem

--- PHP/base.php
+++ PHP/base.php
@@ -40,30 +40,58 @@ class BaseConnection
 
     //--------------------------- utils section -----------------------------------------------------
     // pageNumber start from 1
-    public function getListApp(/*$pageNumber*/) {
+    public function getListApp(/*$pageNumber, $pageFilter*/) {
+        $json = array(
+            'maxRecords'        => 0,
+            'filteredRecords'   => 0,
+            'html'              => ''
+        );
+
         $pageNumber = $_GET['pageNumber'];
-
         $sql = 'SELECT id, main_image, name, downloads, activity FROM `apkshki_full_base_txt2`';
-
         $result = $this->getQuery($sql);
+        $rowsCount = mysqli_num_rows($result);
+        $json['maxRecords'] = $rowsCount;
+        
+        $pageFilter = $_GET['pageFilter'];
+        if ($pageFilter){
+            if ($pageFilter == 'games'){
+                $sql .= ' WHERE category LIKE "Игры>%"';
+            }
+            else if ($pageFilter == 'apps'){
+                $sql .= ' WHERE category LIKE "Приложения>%"';
+            }
+            else if (strlen($pageFilter) > 0 and $pageFilter != 'all'){
+                $sql .= ' WHERE category = "' . $pageFilter . '"';
+            }
+            $result = $this->getQuery($sql);
+            $rowsCount = mysqli_num_rows($result);
+
+            $json['filteredRecords'] = $rowsCount;
+        }
     
         $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        $rowsCount = mysqli_num_rows($result);
+        
+        $html = '';
     
-        for ($i = ($pageNumber - 1) * 20; $i < $pageNumber * 20; $i++) {
+        for ($i = ($pageNumber - 1) * 20; ($i < $pageNumber * 20 && $i < $rowsCount); $i++) {
             $row = $rows[$i];
-            echo '<div class="bodyOfListApplication__item" id="' . $row['id']  .'">';
-            echo '  <div class="bodyOfListApplication__item__image"><img src="' . $row['main_image'] . '" width="90" height="90"></div>';
-            echo '  <div class="bodyOfListApplication__item__name">' . $row['name'] . '</div>';
-            echo '  <div class="bodyOfListApplication__item__downloads">' . $row['downloads'] . '</div>';
-            echo '  <div class="bodyOfListApplication__item__activity">';
-            echo '      <div class="button__on__off"></div>';
-            echo '      <div class="button__on__off__text">ON OFF</div>';
-            echo '      <div class="button__on__off__turn ' . (($row['activity'] == '1') ? 'button__on__off__turn_on' : 'button__on__off__turn_off') . '"></div>';
-            echo '  </div>';
-            echo '  <div class="bodyOfListApplication__item__delete"></div>';
-            echo '</div>';
-        }        
+            $html .= '<div class="bodyOfListApplication__item" id="' . $row['id']  .'">';
+            $html .= '  <div class="bodyOfListApplication__item__image"><img src="' . $row['main_image'] . '" width="90" height="90"></div>';
+            $html .= '  <div class="bodyOfListApplication__item__name">' . $row['name'] . '</div>';
+            $html .= '  <div class="bodyOfListApplication__item__downloads">' . $row['downloads'] . '</div>';
+            $html .= '  <div class="bodyOfListApplication__item__activity">';
+            $html .= '      <div class="button__on__off"></div>';
+            $html .= '      <div class="button__on__off__text">ON OFF</div>';
+            $html .= '      <div class="button__on__off__turn ' . (($row['activity'] == '1') ? 'button__on__off__turn_on' : 'button__on__off__turn_off') . '"></div>';
+            $html .= '  </div>';
+            $html .= '  <div class="bodyOfListApplication__item__delete"></div>';
+            $html .= '</div>';
+        }
+
+        $json['html'] = $html;
+
+        echo json_encode($json);
     }
 
     public function setActivityApp(/*$appId, $appActivity*/) {
@@ -81,6 +109,53 @@ class BaseConnection
         $sql = 'DELETE FROM `apkshki_full_base_txt2` WHERE id = ' . $appId;
 
         $result = $this->getQuery($sql);
+    }
+
+    public function getAppInfo(/*$appId*/) {
+        $appId = $_GET['appId'];
+
+        $sql = 'SELECT * FROM `apkshki_full_base_txt2` WHERE id = ' . $appId;
+
+        $result = $this->getQuery($sql);
+
+        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+        echo json_encode($rows[0]);
+    }
+
+
+    public function getGameList() {
+        $sql = 'SELECT category FROM `apkshki_game_category`';
+
+        $result = $this->getQuery($sql);
+
+        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $rowsCount = mysqli_num_rows($result);
+    
+        $resultList = array();
+        for ($i = 0; $i < $rowsCount; $i++) {
+            $row = $rows[$i];
+            $resultList[$i] = $row['category'];
+        }
+
+        echo json_encode($resultList);
+    }
+
+    public function getAppList() {
+        $sql = 'SELECT category FROM `apkshki_app_category`';
+
+        $result = $this->getQuery($sql);
+
+        $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        $rowsCount = mysqli_num_rows($result);
+    
+        $resultList = array();
+        for ($i = 0; $i < $rowsCount; $i++) {
+            $row = $rows[$i];
+            $resultList[$i] = $row['category'];
+        }
+
+        echo json_encode($resultList);
     }
 
 
